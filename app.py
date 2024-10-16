2028378bb922874a1f9358923876616217a313ae
Vimal Kumar <vkvimal14@gmail.com>
2024-10-17 01:37:16 +0530

    some experiment

--- app.py
+++ app.py
@@ -39,7 +39,7 @@ flow = Flow.from_client_config(
         "web": {
             "client_id": GOOGLE_CLIENT_ID,
             "client_secret": GOOGLE_CLIENT_SECRET,
-            "redirect_uris": [REDIRECT_URI],
+            #"redirect_uris": [REDIRECT_URI],
             "auth_uri": "https://accounts.google.com/o/oauth2/auth",
             "token_uri": "https://oauth2.googleapis.com/token",
         }
